fb0db757d4cc04e26e8eb00d6d925eef4cac7a64
Winder Díaz <winderdiaz29@gmail.com>
2022-01-26 19:15:38 -0400

    Commit inicial

---
+++ multiplicar/multiplicar.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const colors = require('colors/safe');
+
+let listarTabla = (base, limite = 10) => {
+    console.log('====================='.green);
+    console.log(`    Tabla del ${base}`.green);
+    console.log('====================='.green);
+
+    for (let i = 1; i <= limite; i++) {
+        console.log(`${ base } * ${ i } = ${ base * i }`);
+    }
+}
+
+let crearArchivo = (base, limite = 10) => {
+    return new Promise (( resolve, reject ) => {
+
+        if (!Number(base)) {
+            reject(`El valor introducido para la base (${ base }) no es un número`);
+            return;
+        } else if (!Number(limite)) {
+            reject(`El valor introducido para el limite (${ limite }) no es un número`);
+            return
+        }
+
+
+        let data = '';
+
+        for (let i = 1; i <= limite; i++) {
+            data += `${ base } * ${ i } = ${ base * i }\n`;
+        }
+
+        fs.writeFile(`tablas/tabla-${ base }-al-${ limite }.txt`, data, err => {
+            if (err) {
+                throw err;
+            } else {
+                resolve(colors.green(`tabla-${ base }-al-${ limite }`))
+            }
+            console.log();
+        } )
+    })
+}
+
+module.exports = {
+    crearArchivo,
+    listarTabla
+}
+
